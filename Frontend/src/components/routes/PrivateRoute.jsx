c5b9b07505da08bbc828977971f3cdd6aa87e461
gab <gabcam071118.rg@gmail.com>
2025-05-05 16:54:37 -0600

    Refactor PrivateRoute to include role-based access control and loading state handling

--- Frontend/src/components/routes/PrivateRoute.jsx
+++ Frontend/src/components/routes/PrivateRoute.jsx
@@ -2,8 +2,21 @@ import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 
-export default function PrivateRoute({ children }) {
-  const { accessToken } = useContext(AuthContext);
+export default function PrivateRoute({ children, roleRequired }) {
+  const { accessToken, user , isLoading } = useContext(AuthContext);
+
+  if (isLoading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (!user || !accessToken) {
+    return <Navigate to={roleRequired === 2 ? '/admin' : '/'} replace />;
+  }
+
+  if (user.id_rol !== roleRequired) {
+    return <Navigate to={user.id_rol === 1 ? '/home' : '/admin'} replace />;
+  }
 
   return accessToken ? children : <Navigate to="/" />;
 }
+
