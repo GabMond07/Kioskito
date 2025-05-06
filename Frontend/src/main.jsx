1763b65b0fce0c99198295879f90c3d0b7ad3fa8
gab <gabcam071118.rg@gmail.com>
2025-05-05 22:52:32 -0600

    Add react-hot-toast for notifications

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <App />
+      <Toaster position="top-center" />
     </AuthProvider>
   </StrictMode>,
 )
