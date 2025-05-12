e5a621abf479e30c7bf615d79d1abe5b4e0c22db
HDLGOJENN <hdlgojenn@gmail.com>
2025-05-12 15:48:56 -0600

    agregue libros al homeview

--- Frontend/src/views/HomeView.jsx
+++ Frontend/src/views/HomeView.jsx
@@ -4,43 +4,67 @@ import Libro2 from "../assets/El-Principito.jpeg";
 import Libro3 from "../assets/Alas-de-sangre.jpeg";
 import Libro4 from "../assets/A-Game-of-Thrones.jpeg";
 import Libro5 from "../assets/Metamorfosis.jpeg";
+import Libro6 from "../assets/geografia.jpg";
+import Libro7 from "../assets/twilight-breaking-dawn.jpg";
+import Libro8 from "../assets/latregua.jpeg";
 
 function HomeView() {
   return (
     <main className="py-5 px-6 md:px-10 min-h-screen">
-        {/* Seccion de continuar libros */}
-        <section>
-        <h2 className="text-xl font-bold text-gray-900 mb-5">Continuar leyendo</h2>
-          <div className="grid grid-cols-2 md:grid-cols-5 gap-3">
-          <Libro
-                      imagen={Libro1}
-                      autor="Julia Quinn"
-                      titulo="El corazón de una Bridgerton"
-                    />
-                    <Libro
-                      imagen={Libro2}
-                      autor="Antoine de Saint"
-                      titulo="El Principito"
-                    />
-                    <Libro
-                      imagen={Libro3}
-                      autor="Rebecca Yarros"
-                      titulo="Alas de Sangre"
-                    />
-        </div> <br />
-        </section>
-        {/* Seccion de libros recomendados */}
-        <section>
-        <h2 className="text-xl font-bold text-gray-900 mb-5">Tu proxima historia</h2> <br />
+      {/* Seccion de continuar libros */}
+      <section>
+        <h2 className="text-xl font-bold text-gray-900 mb-5">
+          Continuar leyendo
+        </h2>
         <div className="grid grid-cols-2 md:grid-cols-5 gap-3">
-         <Libro
-                     imagen={Libro4}
-                     autor="George R. R. Martin"
-                     titulo="A Game of Thrones"
-                   />
-                   <Libro imagen={Libro5} autor="Franz Kafka" titulo="La Metamorfosis" />
+          <Libro
+            imagen={Libro1}
+            autor="Julia Quinn"
+            titulo="El corazón de una Bridgerton"
+          />
+          <Libro
+            imagen={Libro2}
+            autor="Antoine de Saint"
+            titulo="El Principito"
+          />
+          <Libro
+            imagen={Libro3}
+            autor="Rebecca Yarros"
+            titulo="Alas de Sangre"
+          />
+        </div>{" "}
+        <br />
+      </section>
+      {/* Seccion de libros recomendados */}
+      <section>
+        <h2 className="text-xl font-bold text-gray-900 mb-5">
+          Tu proxima historia
+        </h2>{" "}
+        <br />
+        <div className="grid grid-cols-2 md:grid-cols-5 gap-3">
+          <Libro
+            imagen={Libro4}
+            autor="George R. R. Martin"
+            titulo="A Game of Thrones"
+          />
+          <Libro imagen={Libro5} autor="Franz Kafka" titulo="La Metamorfosis" />
+          <Libro
+            imagen={Libro6}
+            autor="George R. R. Martin"
+            titulo="Geografia"
+          />
+          <Libro
+            imagen={Libro7}
+            autor="George R. R. Martin"
+            titulo="Twilight"
+          />
+          <Libro
+            imagen={Libro8}
+            autor="Mario Benedeti"
+            titulo="La Tregua"
+          />
         </div>
-        </section>
+      </section>
     </main>
   );
 }
